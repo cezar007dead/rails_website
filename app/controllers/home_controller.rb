6fc2012084a8709944a4a8a6fd3ea5d1921563cc
Gurgen Meliksetyants <35916442+cezar007dead@users.noreply.github.com>
2021-09-29 15:31:35 -0700

    Form created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+  def calculate_price
+    sqft = params[:sqft].to_i
+    price = params[:pricesqft].to_i
+    @results = calculate(sqft, price)
+  end
+  private
+  def calculate(sqft, price)
+    sqft * price
+  end
 end
