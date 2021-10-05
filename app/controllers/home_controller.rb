df23fe145acdc1090d332c82bb72f18b73491f7d
Gurgen Meliksetyants <35916442+cezar007dead@users.noreply.github.com>
2021-10-05 16:41:50 -0700

    Array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 class HomeController < ApplicationController
   def index
+    users = User.take(10)
+    names = Array.new(10)
+    i = 0
+    for a in users do
+      names[i] = a.first_name
+      i = i + 1
+    end
+    puts "Users Names"
+    puts "#{names}"
   end
   def calculate_price
-    sqft = params[:sqft].to_i
-    price = params[:pricesqft].to_i
+    sqft = params[:area_price][:sqft].to_i
+    price = params[:area_price][:pricesqft].to_i
     @results = calculate(sqft, price)
   end
   private
